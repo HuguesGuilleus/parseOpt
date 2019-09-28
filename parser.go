9a14063e669a07034acc29bb9b9f61a7343ede4f
hugues <ghugues@netc.fr>
2019-09-28 10:10:03 +0200

    parser.go: add ParseOsAndFile()

--- parser.go
+++ parser.go
@@ -18,6 +18,17 @@ func (list *SpecList) ParseArgOs() (opt *Option) {
 	return list.toOption().ParseArgOs()
 }
 
+// Parse Os env, the file and Os arguments, and run the callBack
+func (opt *Option) ParseOsAndFile(file string) (*Option, error) {
+	opt.canRunCB = false
+	opt.ParseOsEnv()
+	err := opt.ParseFile(file)
+	opt.ParseArgOs()
+	opt.canRunCB = true
+	opt.runCB()
+	return opt, err
+}
+
 // Parse the environment variable and argument from OS
 func (opt *Option) ParseOs() *Option {
 	opt.canRunCB = false
